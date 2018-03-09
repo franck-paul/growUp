161042b8c9537b568d664caf529544bd30bfd20e
franck <carnet.franck.paul@gmail.com>
2018-03-09 15:30:40 +0100

    Various code formatting

--- _define.php
+++ _define.php
@@ -1,23 +1,24 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-# This file is part of Gravatar, a plugin for Dotclear 2.
-#
-# Copyright (c) Franck Paul and contributors
-# carnet.franck.paul@gmail.com
-#
-# Licensed under the GPL version 2.0 license.
-# A copy of this license is available in LICENSE file or at
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# -- END LICENSE BLOCK ------------------------------------
+/**
+ * @brief growUp, a plugin for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Plugins
+ *
+ * @author Franck Paul and contributors
+ *
+ * @copyright Franck Paul carnet.franck.paul@gmail.com
+ * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
-if (!defined('DC_RC_PATH')) { return; }
+if (!defined('DC_RC_PATH')) {return;}
 
 $this->registerModule(
-	/* Name */			"growUp",
-	/* Description*/		"Force some upgrades",
-	/* Author */			"Franck Paul",
-	/* Version */			'0.1',
-	array(
-		/* Type */			'type' 			=>	'plugin'
-	)
+    "growUp",              // Name
+    "Force some upgrades", // Description
+    "Franck Paul",         // Author
+    '0.1',                 // Version
+    array(
+        'type' => 'plugin' // Type
+    )
 );
--- _install.php
+++ _install.php
@@ -1,33 +1,34 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-# This file is part of growUp, a plugin for Dotclear 2.
-#
-# Copyright (c) Franck Paul and contributors
-# carnet.franck.paul@gmail.com
-#
-# Licensed under the GPL version 2.0 license.
-# A copy of this license is available in LICENSE file or at
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# -- END LICENSE BLOCK ------------------------------------
+/**
+ * @brief growUp, a plugin for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Plugins
+ *
+ * @author Franck Paul and contributors
+ *
+ * @copyright Franck Paul carnet.franck.paul@gmail.com
+ * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
-if (!defined('DC_CONTEXT_ADMIN')) { return; }
+if (!defined('DC_CONTEXT_ADMIN')) {return;}
 
 $new_version = DC_VERSION; // $core->plugins->moduleInfo('growUp','version');
 $old_version = $core->getVersion('growUp');
 
-if (version_compare($old_version,$new_version,'>=')) return;
+if (version_compare($old_version, $new_version, '>=')) {
+    return;
+}
 
 try
 {
-	if (class_exists('dcUpgrade') && method_exists('dcUpgrade','growUp')) {
-		dcUpgrade::growUp($core,$core->getVersion('core'));
-		$core->setVersion('growUp',$new_version);
+    if (class_exists('dcUpgrade') && method_exists('dcUpgrade', 'growUp')) {
+        dcUpgrade::growUp($core, $core->getVersion('core'));
+        $core->setVersion('growUp', $new_version);
 
-		return true;
-	}
-}
-catch (Exception $e)
-{
-	$core->error->add($e->getMessage());
+        return true;
+    }
+} catch (Exception $e) {
+    $core->error->add($e->getMessage());
 }
 return false;
