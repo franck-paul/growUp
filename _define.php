5b91065d87e6703fdf94db8277c525decc93f719
franck <carnet.franck.paul@gmail.com>
2021-10-23 12:44:09 +0200

    Add alternative repository

--- _define.php
+++ _define.php
@@ -10,16 +10,21 @@
  * @copyright Franck Paul carnet.franck.paul@gmail.com
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-
-if (!defined('DC_RC_PATH')) {return;}
+if (!defined('DC_RC_PATH')) {
+    return;
+}
 
 $this->registerModule(
-    "growUp",              // Name
-    "Force some upgrades", // Description
-    "Franck Paul",         // Author
+    'growUp',              // Name
+    'Force some upgrades', // Description
+    'Franck Paul',         // Author
     '0.2',                 // Version
     [
         'requires' => [['core', '2.13']], // Dependencies
-        'type'     => 'plugin'           // Type
+        'type'     => 'plugin',           // Type
+
+        'details'    => 'https://open-time.net/?q=growUp',       // Details URL
+        'support'    => 'https://github.com/franck-paul/growUp', // Support URL
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/growUp/main/dcstore.xml'
     ]
 );
