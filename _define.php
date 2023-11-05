0b4dddf75581e5ffb5c165c2ad6f46403119d2a4
franck <carnet.franck.paul@gmail.com>
2023-11-05 10:25:34 +0100

    New release 4.2

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'growUp',
     'Force some upgrades',
     'Franck Paul',
-    '4.1',
+    '4.2',
     [
         'requires' => [['core', '2.28']],
         'type'     => 'plugin',
